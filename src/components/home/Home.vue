7e421e364674592504fde212b6d71bc0fdb4cb4c
humbertopiva <contato.humbertopiva@gmail.com>
2020-11-06 16:26:45 -0300

    Finalizado

---
+++ src/components/home/Home.vue
@@ -0,0 +1,120 @@
+<template>
+  <div>
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre por parte do título">
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+
+        <meu-painel :titulo="foto.titulo">
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+          <meu-botao tipo="button" rotulo="REMOVER" @botaoAtivado="remove(foto)" :confirmacao="true" estilo="perigo"/>
+        </meu-painel>
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import Painel from '../shared/painel/Painel.vue';
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue';
+import Botao from '../shared/botao/Botao.vue';
+
+export default {
+
+  components: {
+    'meu-painel' : Painel,
+    'imagem-responsiva' : ImagemResponsiva,
+    'meu-botao' : Botao
+  },
+
+  data() {
+
+    return {
+
+      titulo: 'Alurapic',
+      fotos: [],
+      filtro: ''
+    }
+  },
+
+  computed: {
+
+    fotosComFiltro() {
+
+      if(this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
+    }
+
+  },
+
+  methods: {
+
+    remove(foto) {
+        alert('Remover a foto! ' + foto.titulo);
+    }
+
+  },
+  
+  created(){
+
+    let promise = this.$http.get('http://localhost:3000/v1/fotos');
+    promise
+      .then(res => {res.json()
+      .then(fotos => this.fotos = fotos, err => console.log(err));
+    });
+  }
+}
+
+</script>
+
+<style>
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .filtro{
+    display: block;
+    width: 100%;
+  }
+
+  /* estilo do painel */ 
+
+   .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
+
+</style>
---
+++ src/components/shared/painel/Painel.vue
@@ -0,0 +1,85 @@
+<template>
+        <div class="painel">
+          <h2 class="painel-titulo" @dblclick="visivel = !visivel">{{ titulo }}</h2>
+          <transition name="painel-fade">
+            <div class="painel-conteudo" v-show="visivel" >
+              <slot></slot>
+            </div>
+          </transition>
+        </div>
+</template>
+
+<script>
+
+export default {
+
+    props: ['titulo'],
+
+
+    data() {
+
+      return {
+
+        visivel: true
+      }
+    }
+}
+
+</script>
+
+<style scoped>
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+  
+  /* estilo do painel */ 
+
+   .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
+
+  * {
+    box-shadow: 5px 5px 5px;
+  }
+
+  .painel-fade-enter, .painel-fade-leave-active {
+    opacity: 0;
+  }
+
+  .paine-fade-enter-active, .painel-fade-leave-active{
+    transition: opacity .4s;
+  }
+  
+</style>
